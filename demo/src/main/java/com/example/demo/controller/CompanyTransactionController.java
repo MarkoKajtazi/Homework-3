f5841cac22fd97121596b9fc3388e2c2da0e13aa
MarkoKajtazi <marko.kajtazi@students.finki.ukim.mk>
2024-12-31 03:05:30 +0100

    Add files via upload
    This is the spring boot web app that communicates with the python flask script and populates the database with the necessary data from the scraped csv files

---
+++ demo/src/main/java/com/example/demo/controller/CompanyTransactionController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Company;
+import com.example.demo.model.CompanyTransaction;
+import com.example.demo.service.CSVDataImporter;
+import com.example.demo.service.CompanyService;
+import com.example.demo.service.CompanyTransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/api")
+@Validated
+@CrossOrigin(origins="*")
+public class CompanyTransactionController {
+    private final CompanyTransactionService companyTransactionService;
+    private final CompanyService companyService;
+    private final CSVDataImporter csvDataImporter;
+
+    public CompanyTransactionController(CompanyTransactionService companyTransactionService, CompanyService companyService, CSVDataImporter csvDataImporter) {
+        this.companyTransactionService = companyTransactionService;
+        this.companyService = companyService;
+        this.csvDataImporter = csvDataImporter;
+        companyService.fetchAndSaveCompany();
+//        csvDataImporter.importCSVToDatabase();
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Company>> getAllData() {
+        List<Company> transactions = companyService.getAll();
+        return ResponseEntity.ok(transactions);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CompanyTransaction> getDataById(@PathVariable Long id) {
+        return ResponseEntity.ok(companyTransactionService.getDataById(id));
+    }
+
+    @GetMapping("/companies")
+    public ResponseEntity<List<String>> getAllCompanies() {
+        return ResponseEntity.ok(companyService.getAllCodes());
+    }
+
+    @GetMapping("/transactions/{code}")
+    public ResponseEntity<List<CompanyTransaction>> getDataByCode(@PathVariable String code) {
+        List<CompanyTransaction> transactions = companyService.getTransactionByCompanyCode(code);
+        return ResponseEntity.ok(transactions);
+    }
+
+}
